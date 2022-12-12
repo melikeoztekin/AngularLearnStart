5ed884416586070a696495e9be77c73b124c929f
melikeoztekin <melikeoztekin06@gmail.com>
2022-12-12 22:46:40 +0300

    Authorization interceptor

---
+++ src/app/interceptors/authorization.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthorizationInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const modified = request.clone({
+      headers: request.headers.set('Authorization', 'EXAMPLE-JWT'),
+    });
+    return next.handle(modified);
+  }
+}
