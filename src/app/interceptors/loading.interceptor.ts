710edac922a2ee91be82e7bb7ef9007ca64e8775
melikeoztekin <melike.oztekin@etiya.com>
2022-12-16 09:14:05 +0300

    authorization interceptor

--- src/app/interceptors/loading.interceptor.ts
+++ src/app/interceptors/loading.interceptor.ts
@@ -6,10 +6,11 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { finalize, Observable } from 'rxjs';
+import { LoadingService } from '../services/loadingService/loading.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private loadingService: LoadingService) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -18,14 +19,18 @@ export class LoadingInterceptor implements HttpInterceptor {
     //# Requesti ve response2u manipüle etme
     //# Eğer uygulamada o an bir request gönderilmiş ve cevap bekleniyorsa, uygulama loadinng ekranı gösterilsin
     console.log('Loading started');
+
+    this.loadingService.startLoading('Merhaba melike');
+
     //# cevap  geldiğinde => loading stoped
     //# rxjs
+
     // TODO Add loading event
     return next.handle(request).pipe(
       finalize(() => {
         //# requestin response döndüğü ve sonlandığını ele alan fonksiyon
-
         console.log('request bittiğiinde çalışacak fonksiyon');
+        this.loadingService.stopLoading();
       })
     );
   }
